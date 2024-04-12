08f4d992dc27a72410182825d39f343f4c3b0aa7
Jadeeeey <852367891@qq.com>
2024-04-12 17:58:20 +0800

    add experiment

--- inference.py
+++ inference.py
@@ -1,13 +1,20 @@
 import torch
 from diffusers import DiffusionPipeline
 
-# model_id = "<Your Model Path>"
-model_id = "/home/users/nus/e1216290/scratch/colossalai/ColossalAI/examples/images/dreambooth/weight_output"
-print(f"Loading model... from{model_id}")
+model_id = "./model/stable-diffusion-v1-4"
+output_path = "./"
+print(f"Loading original model... from{model_id}")
 
 pipe = DiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16).to("cuda")
 
-prompt = "A photo of a dog in Starbucks"
+prompt = "a photo of kudoshinichi in space"
 image = pipe(prompt, num_inference_steps=50, guidance_scale=7.5).images[0]
+image.save(f"{output_path}output_original.png")
 
-image.save("/home/users/nus/e1216290/scratch/colossalai/ColossalAI/examples/images/dreambooth/output.png")
+
+print(f"Loading finetuned model... from{model_id}")
+state_dict = torch.load("./weight_output/diffusion_pytorch_model.bin")
+pipe.unet.load_state_dict(state_dict)
+
+image = pipe(prompt, num_inference_steps=50, guidance_scale=7.5).images[0]
+image.save(f"{output_path}output.png")
